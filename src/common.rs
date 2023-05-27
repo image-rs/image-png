6f2522aec7756b32ca1b742e498d6ef2e4fb5d45
dAxpeDDa <daxpedda@gmail.com>
2023-05-27 15:56:04 +0200

    Bump `bitflags` to v2

--- src/common.rs
+++ src/common.rs
@@ -725,6 +725,7 @@ bitflags! {
     const SCALE_16            = 0x8000; // read only
     ```
     "]
+    #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
     pub struct Transformations: u32 {
         /// No transformation
         const IDENTITY            = 0x0000; // read and write */

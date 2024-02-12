c1f7c92a86e6f15aa8128299d9117245af5dae74
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-12 23:25:26 +0000

    Update the `bitflags` dependency to `2.x.x`.
    Tested by:
    
    * `cargo test`
    * Running `cargo doc --open` and checking if `Transformations`
      implements a superset of the traits that it has implemented before.
      Additional traits: `bitflags::Flags`, `IntoIterator`.

--- src/common.rs
+++ src/common.rs
@@ -740,6 +740,7 @@ bitflags! {
     const SCALE_16            = 0x8000; // read only
     ```
     "]
+    #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
     pub struct Transformations: u32 {
         /// No transformation
         const IDENTITY            = 0x00000; // read and write */
